40054b1d574c300cb4e23c47b9cf67feb7f7f50c
unknown <reinaldosaopaulo1@hotmail.com>
2024-12-11 17:14:28 -0300

    Criando metodos de comunicação com o banco

---
+++ lib/data/database.dart
@@ -0,0 +1,12 @@
+
+import 'package:nosso_primeiro_projeto/data/task_dao.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+Future<Database> getDatabsae() async {
+  final String path = join(await getDatabasesPath(), 'task.db');
+  return openDatabase(path, onCreate: (db, version) {
+    db.execute(TaskDao.tableSql);
+  }, version: 1);
+}
+
